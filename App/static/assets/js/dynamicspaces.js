8529202f876ebe049d3ef81c6804a3fe79609a74
goutham <30994768+MGouthamB@users.noreply.github.com>
2023-08-03 20:11:05 -0400

    iframe

--- App/static/assets/js/dynamicspaces.js
+++ App/static/assets/js/dynamicspaces.js
@@ -1,9 +1,12 @@
+var isdataread=false;
 window.addEventListener('message', function(event) {
-  if (event.data && event.data.type === 'iframeHeight') {
-    var iframe = document.getElementById('your-iframe-id');
+  if (event.data && event.data.type === 'iframeHeight' && !isdataread) {
+    var iframe = document.getElementById('DSiframe');
     if (iframe) {
       iframe.style.height = event.data.height+'px';
       alert(event.data.height);
+      isdataread=true;
+
     }
   }
 });
