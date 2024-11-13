5c4b996b08f8fef247304fa27813235bd3cf682a
goutham <30994768+MGouthamB@users.noreply.github.com>
2024-11-13 06:57:14 -0500

    DS-58

--- App/static/assets/js/dynamicspaces.js
+++ App/static/assets/js/dynamicspaces.js
@@ -1,6 +1,6 @@
 var isdataread=false;
 window.addEventListener('message', function(event) {
-  if (event.data && event.data.type === 'iframeHeight' && !isdataread) {
+  if (event.data.height) {
     var iframe = document.getElementById('DSiframe');
     if (iframe) {
       iframe.style.height = event.data.height+'px';
